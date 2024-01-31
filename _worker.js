2f0da22007cf19a5840bcf3b156e13049af16021
Michik234 <143963329+Michik234@users.noreply.github.com>
2024-01-31 23:25:39 +0700

    Bedit-Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '6afc97a5-0c65-4705-ba48-7022cc7f2cca';
 
-const proxyIPs = ['47.91.30.39'];
+const proxyIPs = ['8.222.144.117'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
