b854a93851653c8934c850b872e3413e3ba91176
Michik234 <143963329+Michik234@users.noreply.github.com>
2024-02-12 15:40:25 +0700

    USA-Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '6afc97a5-0c65-4705-ba48-7022cc7f2cca';
 
-const proxyIPs = ['103.253.25.59'];
+const proxyIPs = ['164.152.111.148'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
